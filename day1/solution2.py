1d6ae76cd130a137bc305897dba27d97054b94a5
Yorrick Bakker <y.bakker@me.com>
2020-12-01 20:47:28 +0100

    Add second solution with reduced complexity

---
+++ day1/solution2.py
@@ -0,0 +1,43 @@
+# Trying to reduce the complexity by using python set
+
+import os
+import time
+
+
+# O(n)
+def step1(sumValue, arr):
+    memory = set()
+    for i, val in enumerate(arr):
+        remainder = sumValue - val
+        if remainder < 0: continue
+
+        if remainder in memory:
+            return val * remainder
+        memory.add(val)
+
+
+# O(n^2)
+def step2(sumValue, arr):
+    for i, val in enumerate(arr):
+        remainder = sumValue - val
+        if remainder < 0: continue
+
+        result = step1(remainder, arr)
+        if result is not None:
+            return val * result
+
+
+puzzleInput = open(f'{os.getcwd()}/day1/input.txt').read().split('\n')
+puzzleInput = [int(num) for num in puzzleInput]
+
+startTime = time.time()
+
+print(step1(2020, puzzleInput))
+
+print(f'Step 1 execution time: {(time.time() - startTime) * 1000}ms')
+
+startTime = time.time()
+
+print(step2(2020, puzzleInput))
+
+print(f'Step 2 execution time: {(time.time() - startTime) * 1000}ms')
