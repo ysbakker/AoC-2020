586cf1b6d79e350f974c5e343b2516e417c9b400
Yorrick Bakker <y.bakker@me.com>
2020-12-05 11:54:29 +0100

    Add day 5 solutions

---
+++ day5/solution.py
@@ -0,0 +1,43 @@
+# https://adventofcode.com/2020/day/5
+
+import os, time
+
+
+def parseBinary(inpt):
+    return int(
+        inpt.replace('B', '1').replace('R',
+                                       '1').replace('F',
+                                                    '0').replace('L', '0'), 2)
+
+
+def getSeats(inpt):
+    seats = set()
+    for chair in inpt:
+        row, col = (parseBinary(chair[:7]), parseBinary(chair[7:]))
+        seats.add(row * 8 + col)
+    return seats
+
+
+def step1(puzzleInput):
+    return max(getSeats(puzzleInput))
+
+
+def step2(puzzleInput):
+    seats = getSeats(puzzleInput)
+    for i in range(127 * 8 + 7):
+        if i not in seats:
+            if i + 1 in seats and i - 1 in seats:
+                return i
+
+
+with open(f'{os.getcwd()}/day5/input') as inputFile:
+    puzzleInput = inputFile.read().split('\n')
+    startTime = time.time()
+    print(step1(puzzleInput))
+    print(f'Step 1 execution time: {(time.time() - startTime) * 1000}ms')
+    # ~ 2-4 ms
+
+    startTime = time.time()
+    print(step2(puzzleInput))
+    print(f'Step 2 execution time: {(time.time() - startTime) * 1000}ms')
+    # ~ 2-4 ms
