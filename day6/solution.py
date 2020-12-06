82f15a2423be3f0393afd6180977439e9d6db4e3
Yorrick Bakker <y.bakker@me.com>
2020-12-06 13:00:25 +0100

    Add day 6 solutions

---
+++ day6/solution.py
@@ -0,0 +1,43 @@
+# https://adventofcode.com/2020/day/6
+
+import os, time
+
+
+def uniqueAnswers(groupAnswers):
+    unique = 0
+    for answer in groupAnswers:
+        unique += len(set(answer.replace('\n', '')))
+    return unique
+
+
+def unanimousAnswers(groupAnswers):
+    unanimous = 0
+    for group in groupAnswers:
+        memberAnswers = group.split('\n')
+        currentlyUnanimous = set(memberAnswers[0])
+        for i, answers in enumerate(memberAnswers):
+            if i == 0: continue
+            currentlyUnanimous = currentlyUnanimous & set(answers)
+        unanimous += len(currentlyUnanimous)
+    return unanimous
+
+
+def step1(puzzleInput):
+    return uniqueAnswers(puzzleInput)
+
+
+def step2(puzzleInput):
+    return unanimousAnswers(puzzleInput)
+
+
+with open(f'{os.getcwd()}/day6/input') as inputFile:
+    puzzleInput = inputFile.read().split('\n\n')
+    startTime = time.time()
+    print(step1(puzzleInput))
+    print(f'Step 1 execution time: {(time.time() - startTime) * 1000}ms')
+    # ~ 1 ms
+
+    startTime = time.time()
+    print(step2(puzzleInput))
+    print(f'Step 2 execution time: {(time.time() - startTime) * 1000}ms')
+    # ~ 2-3 ms
