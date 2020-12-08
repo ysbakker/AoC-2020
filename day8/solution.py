d7fb38b9a415334c749d1c097d9678a881defc53
Yorrick Bakker <y.bakker@me.com>
2020-12-08 12:04:32 +0100

    Refactor day 8 solution

--- day8/solution.py
+++ day8/solution.py
@@ -7,10 +7,8 @@ def runBootCode(instructions, mem=[0, 0, set()]):
     mem[2].add(mem[0])
     op, val = instructions[mem[0]]
 
-    if op == 'jmp':
-        mem[0] += val - 1
-    elif op == 'acc':
-        mem[1] += val
+    if op == 'jmp': mem[0] += val - 1
+    elif op == 'acc': mem[1] += val
 
     mem[0] += 1
 
@@ -28,24 +26,17 @@ def step2(puzzleInput):
     instructions = [[i.split(' ')[0], int(i.split(' ')[1])]
                     for i in puzzleInput.split('\n')]
 
-    result = 0
-
     for cursor, instruction in enumerate(instructions):
         op, val = instruction
         if op == 'acc': continue
-        instructions[cursor][0] = 'jmp' if op == 'nop' else 'nop'
+        instruction[0] = 'jmp' if op == 'nop' else 'nop'
 
         mem = [0, 0, set()]  # [cursor, accumulator, seen]
         while mem[0] not in mem[2] and mem[0] != len(instructions):
             runBootCode(instructions, mem)
 
-        if mem[0] == len(instructions):
-            result = mem[1]
-            break
-        instructions[cursor][
-            0] = 'jmp' if instructions[cursor][0] == 'nop' else 'nop'
-
-    return result
+        if mem[0] == len(instructions): return mem[1]
+        instruction[0] = 'jmp' if instruction[0] == 'nop' else 'nop'
 
 
 with open(f'{os.getcwd()}/day8/input') as inputFile:
@@ -54,7 +45,7 @@ with open(f'{os.getcwd()}/day8/input') as inputFile:
     print(step2(puzzleInput))
     print(
         f'Step 1 avg: {timeit.timeit(lambda: step1(puzzleInput), number=1000)}ms'
-    )  # 4-6ms
+    )  # <1ms
     print(
         f'Step 2 avg: {timeit.timeit(lambda: step2(puzzleInput), number=1000)}ms'
-    )  # 3-5ms
+    )  # ~10ms
