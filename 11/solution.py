5a46b201c5c19467046eeadff362ef06cc7894d5
Yorrick Bakker <y.bakker@me.com>
2020-12-11 22:44:56 +0100

    Fix day 11 part 1

--- 11/solution.py
+++ 11/solution.py
@@ -9,12 +9,11 @@ def step1(puzzleInput):
     done = False
     while not done:
         newArrangement = []
+        done = True
         for r, row in enumerate(arrangement):
-            done = True
-            newRow = row
+            newRow = ''
             for s, seat in enumerate(row):
                 adjacent = 0
-                if seat == '.': continue
                 for y in (-1, 0, 1):
                     for x in (-1, 0, 1):
                         if x == y == 0: continue
@@ -23,12 +22,14 @@ def step1(puzzleInput):
                             if arrangement[r + y][s + x] == '#': adjacent += 1
                 if adjacent == 0 and seat == 'L':
                     done = False
-                    newRow = newRow[:s] + '#' + newRow[s + 1:]
+                    newRow += '#'
                     occupied += 1
-                if adjacent >= 4 and seat == '#':
+                elif adjacent >= 4 and seat == '#':
                     done = False
-                    newRow = newRow[:s] + 'L' + newRow[s + 1:]
+                    newRow += 'L'
                     occupied -= 1
+                else:
+                    newRow += seat
             newArrangement.append(newRow)
         arrangement = newArrangement
     return occupied
@@ -43,8 +44,8 @@ with open(f'{os.getcwd()}/11/input') as inputFile:
     print(step1(puzzleInput))
     print(step2(puzzleInput))
     print(
-        f'Step 1 avg: {timeit.timeit(lambda: step1(puzzleInput), number=1000)}ms'
+        f'Step 1 avg: {timeit.timeit(lambda: step1(puzzleInput), number=1) * 1000 }ms'
     )
     print(
-        f'Step 2 avg: {timeit.timeit(lambda: step2(puzzleInput), number=1000)}ms'
+        f'Step 2 avg: {timeit.timeit(lambda: step2(puzzleInput), number=1) * 1000 }ms'
     )
