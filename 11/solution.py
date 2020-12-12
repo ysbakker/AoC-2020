0a92200d524a173897ad052ff519a69792eb21ad
Yorrick Bakker <y.bakker@me.com>
2020-12-12 12:38:04 +0100

    Refactor day 11
    This is literally me after writing this: 🤡

--- 11/solution.py
+++ 11/solution.py
@@ -4,8 +4,8 @@
 import os, timeit
 
 
-def step1(puzzleInput):
-    arrangement = puzzleInput.split('\n')
+def solve(inp, part):
+    arrangement = inp.split('\n')
     occupied = 0
     done = False
     while not done:
@@ -18,51 +18,28 @@ def step1(puzzleInput):
                 for y in (-1, 0, 1):
                     for x in (-1, 0, 1):
                         if x == y == 0: continue
-                        if 0 <= r + y < len(arrangement) and 0 <= s + x < len(
-                                arrangement[0]):
-                            if arrangement[r + y][s + x] == '#': adjacent += 1
+                        if part == 1:
+                            if 0 <= r + y < len(
+                                    arrangement) and 0 <= s + x < len(
+                                        arrangement[0]):
+                                if arrangement[r + y][s + x] == '#':
+                                    adjacent += 1
+                        if part == 2:
+                            i = 1
+                            while 0 <= r + i * y < len(
+                                    arrangement) and 0 <= s + i * x < len(
+                                        arrangement[0]):
+                                if arrangement[r + i * y][s + i * x] == '#':
+                                    adjacent += 1
+                                if arrangement[r + i * y][s + i * x] != '.':
+                                    break
+                                i += 1
                 if adjacent == 0 and seat == 'L':
                     done = False
                     newRow += '#'
                     occupied += 1
-                elif adjacent >= 4 and seat == '#':
-                    done = False
-                    newRow += 'L'
-                    occupied -= 1
-                else:
-                    newRow += seat
-            newArrangement.append(newRow)
-        arrangement = newArrangement
-    return occupied
-
-
-def step2(puzzleInput):
-    arrangement = puzzleInput.split('\n')
-    occupied = 0
-    done = False
-    while not done:
-        newArrangement = []
-        done = True
-        for r, row in enumerate(arrangement):
-            newRow = ''
-            for s, seat in enumerate(row):
-                adjacent = 0
-                for y in (-1, 0, 1):
-                    for x in (-1, 0, 1):
-                        if x == y == 0: continue
-                        i = 1
-                        while 0 <= r + i * y < len(
-                                arrangement) and 0 <= s + i * x < len(
-                                    arrangement[0]):
-                            if arrangement[r + i * y][s + i * x] == '#':
-                                adjacent += 1
-                            if arrangement[r + i * y][s + i * x] != '.': break
-                            i += 1
-                if adjacent == 0 and seat == 'L':
-                    done = False
-                    newRow += '#'
-                    occupied += 1
-                elif adjacent >= 5 and seat == '#':
+                elif seat == '#' and ((part == 1 and adjacent >= 4) or
+                                      (part == 2 and adjacent >= 5)):
                     done = False
                     newRow += 'L'
                     occupied -= 1
@@ -75,11 +52,11 @@ def step2(puzzleInput):
 
 with open(f'{os.getcwd()}/11/input') as inputFile:
     puzzleInput = inputFile.read()
-    print(step1(puzzleInput))
-    print(step2(puzzleInput))
+    print(solve(puzzleInput, 1))
+    print(solve(puzzleInput, 2))
     print(
-        f'Step 1 avg: {timeit.timeit(lambda: step1(puzzleInput), number=1) * 1000 }ms'
+        f'Step 1 avg: {timeit.timeit(lambda: solve(puzzleInput, 1), number=1) * 1000 }ms'
     )
     print(
-        f'Step 2 avg: {timeit.timeit(lambda: step2(puzzleInput), number=1) * 1000 }ms'
+        f'Step 2 avg: {timeit.timeit(lambda: solve(puzzleInput, 2), number=1) * 1000 }ms'
     )
